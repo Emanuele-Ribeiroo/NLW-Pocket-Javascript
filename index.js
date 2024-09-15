f43471d76bd978abed593328ca70257b31648dcc
Emanuele-Ribeiroo <emanueleribeiro882@gmail.com>
2024-09-15 00:00:15 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-//arrays, objetos
 let meta = {
-    value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
+    value: 'Ler um livro por mês',
+    checked: true
 }
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
 
-//function // arrow function
-//const criarMeta = () =>  {}
+let metas = [
+    meta, 
+    {
+        value: "Caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
 
-//function criarMeta() {}
+console.log(metas[1].value)
